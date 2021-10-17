af82fa267308d1f799bf3f3fba21aa14b975cf14
Mario Vidales <devvidales2020@gmail.com>
2021-10-17 09:56:33 -0500

    Avance Estructuras de Control ngIF, ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,112 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
 
 const imageAvatarProperties = {
-  width: 150,
+  width: 300,
+  uploadPercentage:30,
   src:'assets/img/hombre-avatar.jpg'
 }
 
 const imageWorldProperties = {
-  width: 75,
+  width: 300,
+  uploadPercentage:80,
   src:'assets/img/World.jpg'
+}
+
+const btnEnviarProperties = {
+  disabled: true,
+  text: "Enviar"
+}
+const btnIncrementarEdadProperties = {
+  disabled: true,
+  text: "Incrfementar Edad"
+}
+
+const btnActivarProperties = {
+  disabled: false,
+  text: "Activar",
+  Activar: function(){ alert('OK');}
+
+
 }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
   name = 'Nicolas';
+  newName = 'Mario';
   age=21;
   image= 'https://www.unir.net/wp-content/uploads/2020/07/mba-tech.jpg';
   btnDisabled=true;
   imgAvatarProperties = imageAvatarProperties;
   imgWorldProperties = imageWorldProperties;
+  btnEnviarProperties = btnEnviarProperties
+  btnActivarProperties = btnActivarProperties;
+  btnIncrementarEdadProperties = btnIncrementarEdadProperties;
   person = {
-    name: 'Mario',
+    name: 'Mario Alfonso Vidales Vazquéz',
     age : 85,
     avatar: imageAvatarProperties.src
+  };
+  namePersons:string[] = ['Mario', 'Jose','Ana','Felipe','Sandy']
+  ScrollTop=0;
+
+  agregaNombre(){
+
+    if (this.newName)
+    {
+      this.namePersons.push(this.newName)
+      this.newName='';
+    }
+
+  };
+
+  eliminarNombre(pos:number){
+    this.namePersons.splice(pos,1);
+  };
+
+
+
+  Activar() {
+    this.btnEnviarProperties.disabled=!this.btnEnviarProperties.disabled;
+    if (this.btnEnviarProperties.disabled)
+        this.btnActivarProperties.text="Activar";
+       else
+       this.btnActivarProperties.text="Desactivar";
+
+
   }
 
+  Enviar() {
+    alert( 'Enviar Información');
+  }
+
+  increaseAge(){
+      this.person.age++;
+  }
+
+  onScroll(event: Event){
+  const element = event.target as HTMLElement;
+  this.ScrollTop = element.scrollTop;
+  }
+  onKeyUp(event: Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name= element.value;
+  }
 }
+
+
+
